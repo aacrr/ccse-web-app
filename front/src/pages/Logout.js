3f5b5499f057a1eabc1befca2b6d3447cf56ae2f
aacrr <153206009+aacrr@users.noreply.github.com>
2025-05-21 16:58:07 +0100

    Update Logout.js

--- front/src/pages/Logout.js
+++ front/src/pages/Logout.js
@@ -1,13 +1,11 @@
 import React, { useEffect } from "react"
 import { useState } from "react"
-import { json } from "react-router-dom"
 import { useNavigate } from "react-router-dom"
 
 export default function Logout(){
     const nav = useNavigate()
 
     const [verified, setVerified] = useState(false)
-    const [userID, setUserID] = useState(false)
     const checkVerified = async() => {
         const response = await fetch('https://localhost:8141/authenticate_user', {
             method:'POST',
@@ -20,32 +18,8 @@ export default function Logout(){
         const json_response = await response.json()
         if (json_response !== false) {
             setVerified(true)
-            setUserID(json_response)
         } else {
             setVerified(false)
-        }
-    }
-
-    const sendLogout = async() => {
-        const response = await fetch('https://localhost:8141/logout_user', {
-            method: 'POST',
-            headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json'
-            },
-            credentials: 'include'
-        })
-
-        const json_response = await response.json()
-
-        // Return to home page 
-        if (json_response === true) {
-            nav('/')
-            setVerified(false)
-            setUserID(false)
-        }
-        else if (json_response === false) {
-            nav('/')
         }
     }
 
@@ -54,10 +28,31 @@ export default function Logout(){
     }, [])
 
     useEffect(() => {
+        const sendLogout = async() => {
+            const response = await fetch('https://localhost:8141/logout_user', {
+                method: 'POST',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                credentials: 'include'
+            })
+
+            const json_response = await response.json()
+
+            // Return to home page 
+            if (json_response === true) {
+                nav('/')
+                setVerified(false)
+            }
+            else if (json_response === false) {
+                nav('/')
+            }
+        }
         if (verified){
             sendLogout()
         }
-    }, [verified])
+    }, [verified, nav])
 
     return (
         <>
